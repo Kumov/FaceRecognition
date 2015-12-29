58d824a9b9f5fa2065fba9a0dc00b417c7dc4a2d
kiddos <josephyu831007@yahoo.com.tw>
2015-12-29 15:54:27 +0800

    setup currentModelPath name and create image root directory if not exists

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define MODEL_BASE_NAME "facemodel"
+#define MODEL_EXTENSION ".xml"
+
 MainWindow::MainWindow(QWidget *parent) :
             QMainWindow(parent),
             ui(new Ui::MainWindow),
@@ -14,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
   timer->start(INTERVAL);
   connect(timer, SIGNAL(timeout()), this, SLOT(setImage()));
+  connect(ui->trainButton, SIGNAL(pressed()), SLOT(train()));
 }
 
 MainWindow::~MainWindow() {
@@ -22,11 +26,11 @@ MainWindow::~MainWindow() {
   if (ui != nullptr)
     delete ui;
   if (mainDisplay != nullptr)
-      delete mainDisplay;
+    delete mainDisplay;
   if (faceDisplay != nullptr)
-      delete faceDisplay;
+    delete faceDisplay;
   if (classifier != nullptr)
-      delete classifier;
+    delete classifier;
 }
 
 void MainWindow::setImage() {
@@ -39,3 +43,22 @@ void MainWindow::setImage() {
     faceDisplay->setImage(face);
   }
 }
+
+void MainWindow::train() {
+  QDir imageRoot(FACE_DATA_DIRECTORY);
+  if (!imageRoot.exists()) {
+    imageRoot.mkpath(".");
+  }
+  setupTraining();
+}
+
+void MainWindow::setupTraining() {
+  QDateTime currenTime = QDateTime::currentDateTime();
+  currentModelPath = QString(MODEL_BASE_NAME) +
+          QString::number(currenTime.toTime_t()) +
+          QString(MODEL_EXTENSION);
+  cout << currentModelPath.toStdString() << endl;
+}
+
+#undef MODEL_BASE_NAME
+#undef MODEL_EXTENSION
