629a04b38a9ab6802ee1369c8f687949ee14b3ea
kiddos <josephyu831007@yahoo.com.tw>
2016-01-01 01:35:45 +0800

    move constants to mainwindow class

--- mainwindow.h
+++ mainwindow.h
@@ -11,6 +11,17 @@
 #include "opencvcamera.h"
 #include "imageviewer.h"
 #include "trainingtask.h"
+#include "classifier.h"
+
+#define FACE_IMAGE_ROOT_DIR "faces"
+#define FACE_MODEL_BASE_NAME "facemodel"
+#define FACE_MODEL_EXTENSION ".xml"
+#define INTERVAL 33
+#define PERCENT 0.9
+
+using classifier::FaceClassifier;
+using classifier::FeatureType;
+using cv::Mat;
 
 namespace Ui {
 class MainWindow;
@@ -24,7 +35,10 @@ class MainWindow : public QMainWindow {
  public slots:
   void setImage();
   void train();
-  void trainingComplete(QString modelPath, QMap<int, QString> names);
+  void trainingComplete(QString modelPath,
+                        QMap<int, QString> names);
+  void takePicture();
+  void resume();
   void setLog(QString log);
  private:
   Ui::MainWindow *ui = nullptr;
@@ -33,7 +47,19 @@ class MainWindow : public QMainWindow {
   ImageViewer* mainDisplay = nullptr;
   ImageViewer* faceDisplay = nullptr;
   TrainingTask* trainingTask = nullptr;
-  const int INTERVAL = 33;
+  FaceClassifier* faceClassifier = nullptr;
+  QMap<int, QString> names;
+  Mat face;
+  bool pictureTaken = false;
+  const char* FACE_IMAGE_DIR = FACE_IMAGE_ROOT_DIR;
+  const char* MODEL_BASE_NAME = FACE_MODEL_BASE_NAME;
+  const char* MODEL_EXTENSION = FACE_MODEL_EXTENSION;
+  const int CAMEAR_INTERVAL = INTERVAL;
+  const double LOADING_PERCENT = PERCENT;
+  const FeatureType FEATURE_TYPE = classifier::LBP;
 };
+
+#undef FACE_IMAGE_ROOT_DIR
+#undef INTERVAL
 
 #endif // MAINWINDOW_H
