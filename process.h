9014b0fce12e2d65902bade949e9e4393055d979
kiddos <josephyu831007@yahoo.com.tw>
2016-01-04 23:29:59 +0800

    remove cv::

--- process.h
+++ process.h
@@ -1,14 +1,17 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
+using cv::Mat;
 
 namespace process {
-  void changeBrightness(cv::Mat& image, double alpha);
-  void changeBrightness(cv::Mat& image, double alpha, double beta);
-  void rotateImage(cv::Mat& image, const double deg);
-  void computeLBP(cv::Mat& image, cv::Mat& lbp);
+  void changeBrightness(Mat& image, double alpha);
+  void changeBrightness(Mat& image, double alpha, double beta);
+  void rotateImage(Mat& image, const double deg);
+  void computeLBP(Mat& image, Mat& lbp);
+  void computeLTP(Mat& image, Mat& ltp, int threshold);
 }
 
 #endif /* end of include guard: PROCESS_H */
