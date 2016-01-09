059df6e31376568f2c8388c6522c0c98e4c0da56
kiddos <josephyu831007@yahoo.com.tw>
2016-01-09 16:25:06 +0800

    model path for windows

--- src/opencvcamera.h
+++ src/opencvcamera.h
@@ -9,6 +9,15 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/objdetect.hpp>
 
+#define DEFAULT_WIDTH 640
+#define DEFAULT_HEIGHT 480
+
+#if defined(__unix__)
+#define OBJECT_DETECT_MODEL "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml"
+#elif defined(__WIN32)
+#define OBJECT_DETECT_MODEL "./haarcascade_frontalface_alt2.xml"
+#endif
+
 #ifdef QT_DEBUG
 #include <iostream>
 using std::cout;
@@ -29,14 +38,19 @@ class OpenCVCamera {
   QImage getCurrentFrame();
   QImage getCurrentFace();
   void getCurrentFaceMat(Mat& face);
+
  private:
   VideoCapture capture;
   CascadeClassifier faceFinder;
   Mat frame, main, face;
   QImage mainImage, faceImage;
-  const int IMAGE_WIDTH = 640;
-  const int IMAGE_HEIGHT = 480;
-  const char* FACE_FINDER_MODEL = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml";
+  const int IMAGE_WIDTH = DEFAULT_WIDTH;
+  const int IMAGE_HEIGHT = DEFAULT_HEIGHT;
+  const char* FACE_FINDER_MODEL = OBJECT_DETECT_MODEL;
 };
+
+#undef DEFAULT_WIDTH
+#undef DEFAULT_HEIGHT
+#undef OBJECT_DETECT_MODEL
 
 #endif // OPENCVCAMERA_H
