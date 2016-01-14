be74658038f17bdc347bb5a4787834373e4cafca
kiddos <josephyu831007@yahoo.com.tw>
2016-01-14 17:17:23 +0800

    remove unused definition and use const char* const for constants

--- src/common.h
+++ src/common.h
@@ -1,7 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <opencv2/core/core.hpp>
+#include <opencv2/core.hpp>
 
 #include <iostream>
 #include <vector>
@@ -10,40 +10,36 @@
 #include <cstring>
 #include <cmath>
 
+using std::string;
+using std::vector;
+
 #if defined(__unix__)
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #elif defined(__WIN32)
-// code for windows
+#include <dirent.h>
 #include <windows.h>
 #endif
 
-const char* CURRENT_DIR = ".";
-const char* PARENT_DIR = "..";
-
 // separator
 #if defined(__unix__)
-#define   SEPARATOR     "/"
+const char* const SEPARATOR = "/";
 #elif defined(__WIN32)
 // windows implementation ...
-#define   SEPARATOR     "\\"
+const char* const SEPARATOR = "\\";
 #endif
-
-#define VECTOR_FILE "face.vec"
-#define SAMPLE_OUTPUT_FILE "face.list"
-#define BG_OUTPUT_FILE "bg.list"
 
 // define commonly use type
 typedef unsigned int uint32;
 typedef unsigned char uint8;
 
-void scanDir(const std::string path, std::vector<std::string>& files,
-             const std::vector<std::string> exclusion);
+void scanDir(const string path, vector<string>& files,
+             const vector<string> exclusion);
 
-bool fileExists(const std::string filePath);
-uint32_t getLineCount(const std::string filePath);
-bool createDirectory(const std::string name);
-bool deleteFile(const std::string filePath);
+bool fileExists(const string filePath);
+uint32_t getLineCount(const string filePath);
+bool createDirectory(const string name);
+bool deleteFile(const string filePath);
 
 #endif /* end of include guard: COMMON_H */
